7157d112ede4bc2ac0652a3c3282fe88436abbc7
KobetsDev <kobets.dev@gmail.com>
2022-07-29 10:11:53 +0300

    q

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ import dotenv from 'dotenv'
 dotenv.config()
 import { app } from './app.js'
 
+const HOST = '85.193.80.201'
 const PORT = process.env.PORT || 8000
 
-app.listen(PORT, () => {
+app.listen(PORT, HOST, () => {
     console.log(`Server has been started on port ${PORT}`)
 })
